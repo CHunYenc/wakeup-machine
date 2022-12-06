d29734a48819e8144f66d3c0f047a44200cd2d8e
CHunYenc <yuhnoesty0402@hotmail.com.tw>
2022-12-06 21:09:29 +0800

    feat: 凌晨 02:00 - 05:00 不執行.

--- app.py
+++ app.py
@@ -21,8 +21,8 @@ if __name__ == "__main__":
     scheduled_job()
     # 宣告一個排程
     sched = BlockingScheduler(timezone="Asia/Taipei")
-    # 定義排程 : 在周一至周五，每 20 分鐘就做一次 def scheduled_jog()
-    sched.add_job(scheduled_job, "cron", day_of_week="mon-fri", minute="*/10")
+    # 定義排程 : 在周一至周五，並且於 2-5 點睡眠。每 10 分鐘就執行一次 scheduled_jog()
+    sched.add_job(scheduled_job, "cron", day_of_week="mon-fri",hour="0-1,5-23", minute="*/10")
     print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
 
     try:
