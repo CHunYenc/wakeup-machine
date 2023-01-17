14b4366946f94f17c5dabaa52600cbe9155fd0ea
CHunYenc <op84a2546@gmail.com>
2023-01-17 13:16:39 +0800

    feat: add variables.py.

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 from urllib import request
 import os
 import logging
+import variables
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,18 +12,33 @@ logging.basicConfig(
 
 
 def scheduled_job():
-    url = os.environ.get("URL","http://your.domain.com")
-    request.urlopen(url)
-    logging.info(f"連線成功!")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
+    url = variables.URL
+    req = request.Request(url=url, headers=headers)
+    try:
+        request.urlopen(req).read()
+    except Exception as e:
+        logging.info(f"{e}")
+    else:
+        logging.info(f"Successful connection {url}.")
 
 
 if __name__ == "__main__":
-    # 先執行一次
+    # run once
     scheduled_job()
-    # 宣告一個排程
+    # init Scheduler
     sched = BlockingScheduler(timezone="Asia/Taipei")
-    # 定義排程 : 在周一至周五，並且於 2-5 點睡眠。每 10 分鐘就執行一次 scheduled_jog()
-    sched.add_job(scheduled_job, "cron", day_of_week="mon-fri",hour="0-1,5-23", minute="*/10")
+    # add job
+    sched.add_job(scheduled_job, "cron",
+                  year=variables.APSCHEDULER_YEAR,
+                  month=variables.APSCHEDULER_MONTH,
+                  day=variables.APSCHEDULER_DAY,
+                  week=variables.APSCHEDULER_WEEK,
+                  day_of_week=variables.APSCHEDULER_DAY_OF_WEEK,
+                  hour=variables.APSCHEDULER_HOUR,
+                  minute=variables.APSCHEDULER_MINUTE,
+                  second=variables.APSCHEDULER_SECOND)
     print("Press Ctrl+{0} to exit".format("Break" if os.name == "nt" else "C"))
 
     try:
